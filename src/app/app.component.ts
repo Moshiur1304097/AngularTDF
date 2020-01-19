9c3c0fdacd5160489a54c23bed0c5c5665b4acb1
Moshiur Rahman <moshiur.rahman@selise.com.bd>
2020-01-19 20:35:45 +0600

    Two Way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user'
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
  // title = 'TDF';
 topics =['Angular', 'React', 'Vue'];
+
+userModel = new User('Rob', 'rob@test.com', 1737711756,'','morning',true);
+
+onSubmit(userForm){
+  console.log("vbsdk");
+  console.log(userForm);
+  let cur= JSON.stringify(userForm);
+  localStorage.setItem("xxx",cur);
+  let xx=localStorage.getItem("xxx");
+  alert(xx);
+}
+
+
 
 }
