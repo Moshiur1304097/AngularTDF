e983d79a20e1bb95509c3b2b51885f3163af468d
Moshiur Rahman <moshiurrahman6674@gmail.com>
2020-01-22 12:27:51 +0600

    Error Handling in TDF

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from './user'
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,13 @@ export class AppComponent {
 topics =['Angular', 'React', 'Vue'];
 topicHasError=true;
 
+submitted = false;
+
+errorMsg ='';
+
 userModel = new User('', 'rob@test.com', 1737711756,'','morning',true);
+
+constructor(private _enrollmentService: EnrollmentService){}
 
 validateTopic(value){
   if(value==='default'){
@@ -23,15 +30,29 @@ validateTopic(value){
   }
 }
 
-onSubmit(userForm){
-  console.log("vbsdk");
-  console.log(userForm);
-  let cur= JSON.stringify(userForm);
-  localStorage.setItem("xxx",cur);
-  let xx=localStorage.getItem("xxx");
-  alert(xx);
-}
 
+// Local storage
+
+// onSubmit(userForm){
+//   console.log("vbsdk");
+//   console.log(userForm);
+//   let cur= JSON.stringify(userForm);
+//   localStorage.setItem("xxx",cur);
+//   let xx=localStorage.getItem("xxx");
+//   alert(xx);
+// }
+
+
+onSubmit(){
+  this.submitted = true;
+  this._enrollmentService.enroll(this.userModel)
+      .subscribe(
+        data=> console.log('success!',data),
+       // error => console.error('Error!', error)
+       error => this.errorMsg = error.statusText
+      )
+
+}
 
 
 }
